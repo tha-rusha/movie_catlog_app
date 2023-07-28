99286140b4b25e9ac641a01564a237ced8869e13
Tharusha Dilhara <egodagahagedaratharusha2000@gmail.com>
2023-07-29 01:40:04 +0530

    using Map of Movies

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -11,6 +11,62 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  Map<String, Map<String, dynamic>> movieDetails = {
+    'movie1': {
+      'title': 'Inception',
+      'runtime': '148 min',
+      'year': 2010,
+      'imdb': 8.8,
+      'genre': ['Action', 'Adventure', 'Sci-Fi'],
+      'posterUrl': 'https://www.example.com/posters/inception.jpg',
+    },
+    'movie2': {
+      'title': 'The Dark Knight',
+      'runtime': '152 min',
+      'year': 2008,
+      'imdb': 9.0,
+      'genre': ['Action', 'Crime', 'Drama'],
+      'posterUrl': 'https://www.example.com/posters/the_dark_knight.jpg',
+    },
+    'movie3': {
+      'title': 'The Shawshank Redemption',
+      'runtime': '142 min',
+      'year': 1994,
+      'imdb': 9.3,
+      'genre': ['Drama'],
+      'posterUrl':
+          'https://www.example.com/posters/the_shawshank_redemption.jpg',
+    },
+    'movie4': {
+      'title': 'Interstellar',
+      'runtime': '169 min',
+      'year': 2014,
+      'imdb': 8.6,
+      'genre': ['Adventure', 'Drama', 'Sci-Fi'],
+      'posterUrl': 'https://www.example.com/posters/interstellar.jpg',
+    },
+    // Add more movie details here
+    'movie5': {
+      'title': 'Movie 5',
+      'runtime': '120 min',
+      'year': 2022,
+      'imdb': 7.5,
+      'genre': ['Comedy', 'Romance'],
+      'posterUrl': 'https://www.example.com/posters/movie5.jpg',
+    },
+    // Continue adding movies up to movie20
+  };
+
+// You can access the details of each movie using the movie name as the key
+// For example:
+// Map<String, dynamic> movie1Details = movieDetails['movie1'];
+// String title = movie1Details['title'];
+// String runtime = movie1Details['runtime'];
+// int year = movie1Details['year'];
+// double imdb = movie1Details['imdb'];
+// List<String> genres = List.castFrom(movie1Details['genre']);
+// String posterUrl = movie1Details['posterUrl'];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,42 +75,64 @@ class _HomePageState extends State<HomePage> {
             child: Text('Movies',
                 style: TextStyle(color: Colors.white, fontSize: 30))),
       ),
-      body: const Column(children: [
-        SizedBox(
-          height: 20,
-        ),
-        CustomSearchBar(),
-        SizedBox(
-          height: 5,
-        ),
-        CustomFilter(),
-        SizedBox(
-          height: 5,
-        ),
-        Divider(),
-        MovieCard(
-            bgColor: Colors.amber,
-            image: "assets/images/hc.png",
-            title: 'They Cloed Tyrone',
-            genres: 'Action',
-            runtime: '2H 2M',
-            year: 2022,
-            imdb: 6.7),
-        SizedBox(
-          height: 10,
-        ),
-        MovieCard(
-            bgColor: Colors.redAccent,
-            image: "assets/images/flood.png",
-            title: 'The Flood',
-            genres: 'Sci-Fi',
-            runtime: '2H 20M',
-            year: 2023,
-            imdb: 8.5),
-        SizedBox(
-          height: 10,
-        ),
-      ]),
+      body: SingleChildScrollView(
+        child: Column(children: [
+          const SizedBox(
+            height: 20,
+          ),
+          const CustomSearchBar(),
+          const SizedBox(
+            height: 5,
+          ),
+          const CustomFilter(),
+          const SizedBox(
+            height: 5,
+          ),
+          const Divider(),
+          ListView(
+            shrinkWrap: true,
+            physics: const NeverScrollableScrollPhysics(),
+            children: [
+              MovieCard(
+                bgColor: Colors.purple,
+                image: "assets/images/flood.png",
+                title: movieDetails['movie1']?['title'],
+                genres: 'Action',
+                runtime: '2H 2M',
+                year: 2022,
+                imdb: 6.7,
+              ),
+              const MovieCard(
+                bgColor: Colors.blue,
+                image: "assets/images/hc.png",
+                title: 'Avengers',
+                genres: 'Action',
+                runtime: '2H 2M',
+                year: 2022,
+                imdb: 6.7,
+              ),
+              const MovieCard(
+                bgColor: Colors.green,
+                image: "assets/images/hc.png",
+                title: 'Avengers',
+                genres: 'Action',
+                runtime: '2H 2M',
+                year: 2022,
+                imdb: 6.7,
+              ),
+              const MovieCard(
+                bgColor: Colors.amber,
+                image: "assets/images/flood.png",
+                title: 'Avengers',
+                genres: 'Action',
+                runtime: '2H 2M',
+                year: 2022,
+                imdb: 6.7,
+              ),
+            ],
+          )
+        ]),
+      ),
     );
   }
 }
--- lib/widgets/moviecart.dart
+++ lib/widgets/moviecart.dart
@@ -22,105 +22,108 @@ class MovieCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.symmetric(horizontal: 15),
-      height: 200,
-      width: MediaQuery.of(context).size.width,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10),
-        color: bgColor,
-      ),
-      child: Row(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: ClipRRect(
-                borderRadius: BorderRadius.circular(8.0),
-                child: Image.asset(image)),
-          ),
-          const SizedBox(
-            width: 10,
-          ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 20),
-                child: Text(
-                  title,
-                  style: const TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold),
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 15),
+        height: 200,
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: bgColor,
+        ),
+        child: Row(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ClipRRect(
+                  borderRadius: BorderRadius.circular(8.0),
+                  child: Image.asset(image)),
+            ),
+            const SizedBox(
+              width: 10,
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 20),
+                  child: Text(
+                    title,
+                    style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold),
+                  ),
                 ),
-              ),
-              Row(
-                children: [
-                  const Icon(
-                    Icons.movie,
-                    color: Colors.yellow,
-                  ),
-                  const SizedBox(
-                    width: 5,
-                  ),
-                  Text(
-                    'Genres : $genres',
-                    style: const TextStyle(color: Colors.white, fontSize: 16),
-                  ),
-                ],
-              ),
-              Row(
-                children: [
-                  const Icon(
-                    Icons.timer,
-                    color: Colors.yellow,
-                  ),
-                  const SizedBox(
-                    width: 5,
-                  ),
-                  Text(
-                    'Runtime : $runtime',
-                    style: const TextStyle(color: Colors.white, fontSize: 16),
-                  ),
-                ],
-              ),
-              Row(
-                children: [
-                  const Icon(
-                    Icons.calendar_month,
-                    color: Colors.yellow,
-                  ),
-                  const SizedBox(
-                    width: 5,
-                  ),
-                  Text(
-                    'Year :  $year',
-                    style: const TextStyle(color: Colors.white, fontSize: 16),
-                  ),
-                ],
-              ),
-              Row(
-                children: [
-                  const Icon(
-                    Icons.star,
-                    color: Colors.yellow,
-                  ),
-                  const SizedBox(
-                    width: 5,
-                  ),
-                  Text(
-                    'IMDB: $imdb/10',
-                    style: const TextStyle(color: Colors.white, fontSize: 16),
-                  ),
-                ],
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-            ],
-          )
-        ],
+                Row(
+                  children: [
+                    const Icon(
+                      Icons.movie,
+                      color: Colors.yellow,
+                    ),
+                    const SizedBox(
+                      width: 5,
+                    ),
+                    Text(
+                      'Genres : $genres',
+                      style: const TextStyle(color: Colors.white, fontSize: 16),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    const Icon(
+                      Icons.timer,
+                      color: Colors.yellow,
+                    ),
+                    const SizedBox(
+                      width: 5,
+                    ),
+                    Text(
+                      'Runtime : $runtime',
+                      style: const TextStyle(color: Colors.white, fontSize: 16),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    const Icon(
+                      Icons.calendar_month,
+                      color: Colors.yellow,
+                    ),
+                    const SizedBox(
+                      width: 5,
+                    ),
+                    Text(
+                      'Year :  $year',
+                      style: const TextStyle(color: Colors.white, fontSize: 16),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    const Icon(
+                      Icons.star,
+                      color: Colors.yellow,
+                    ),
+                    const SizedBox(
+                      width: 5,
+                    ),
+                    Text(
+                      'IMDB: $imdb/10',
+                      style: const TextStyle(color: Colors.white, fontSize: 16),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
